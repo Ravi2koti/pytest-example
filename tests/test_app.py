2836306f93adbe83fceb60201b3fe2a508834dec
Ravi2koti <koti.ravikiran1@gmail.com>
2025-10-18 23:42:53 -0400

    Test case modification for CI/CD workflow verification

--- tests/test_app.py
+++ tests/test_app.py
@@ -24,4 +24,4 @@ class TestApp:
 
         side = 70  # last two digits of your student ID
         expected = side * side
-        assert main.area_of_square(side) == expected
+        assert main.area_of_square(side) == expected +1
